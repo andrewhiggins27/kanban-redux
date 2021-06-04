340d60dcfa988f97e3190a47f4952290e3848f57
Andrew Higgins <andrewhiggins27@gmail.com>
2021-06-04 15:39:50 -0400

    default state

--- src/ui/Column.js
+++ src/ui/Column.js
@@ -1,11 +1,24 @@
-import React from 'react'
-import { TaskCard } from './TaskCard'
+import React from "react";
+import { TaskCard } from "./TaskCard";
 
-export const Column = () => {
+export const Column = (props) => {
+  const mappedCards = props.column.cards.map((card, index) => {
+    return (
+      <TaskCard
+        key={card.title}
+        card={card}
+        index={index}
+        columnId={props.column.columnId}
+      />
+    );
+  });
+
   return (
     <div className="column box">
-      Column
-      <TaskCard/>
+      <div className="card-header">
+        <div className="card-header-title">{props.column.title}</div>
+      </div>
+      {mappedCards}
     </div>
-  )
-}
+  );
+};
