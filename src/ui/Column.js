889eda4c8f0a37144229d35674ce84062963a029
Andrew Higgins <andrewhiggins27@gmail.com>
2021-06-08 17:03:20 -0400

    display description and expand

--- src/ui/Column.js
+++ src/ui/Column.js
@@ -1,5 +1,5 @@
 import React from "react";
-import EditableLabel from 'react-inline-editing'
+import EditableLabel from "react-inline-editing";
 import { Droppable, Draggable } from "react-beautiful-dnd";
 
 import { TaskCard } from "./TaskCard";
@@ -17,45 +17,53 @@ export const Column = (props) => {
   });
 
   const handleFocusOut = (text) => {
-    const updatedColumn = props.column
-    updatedColumn.title = text
-    const updatedBoard = Array.from(props.board)
-    const foundIndex = updatedBoard.findIndex(col => col.columnId == updatedColumn.columnId)
-    updatedBoard[foundIndex] = updatedColumn
-    
-    props.updateBoard(updatedBoard)
-  }
+    const updatedColumn = props.column;
+    updatedColumn.title = text;
+    const updatedBoard = Array.from(props.board);
+    const foundIndex = updatedBoard.findIndex(
+      (col) => col.columnId === updatedColumn.columnId
+    );
+    updatedBoard[foundIndex] = updatedColumn;
+
+    props.updateBoard(updatedBoard);
+  };
 
   return (
     <Draggable draggableId={props.column.columnId} index={props.index}>
       {(provided) => (
-        <div {...provided.draggableProps} ref={provided.innerRef}>
-          <div className="column box">
-            <div className="card-header">
-              <div className="card-header-title" {...provided.dragHandleProps}>
-                <EditableLabel
-                  text={props.column.title}
-                  onFocusOut={handleFocusOut}
-                >
-                  {props.column.title}
-                </EditableLabel>
-              </div>
+        <div
+          {...provided.draggableProps}
+          ref={provided.innerRef}
+          className="column is-one-quarter"
+        >
+          <div className="card">
+            <div
+              className="card-header-title column-title mb-2"
+              {...provided.dragHandleProps}
+            >
+              <EditableLabel
+                text={props.column.title}
+                onFocusOut={handleFocusOut}
+                className="input"
+              >
+                {props.column.title}
+              </EditableLabel>
             </div>
-            <Droppable droppableId={props.column.columnId} type="card">
-              {(provided, snapshot) => (
-                <div
-                  ref={provided.innerRef}
-                  {...provided.droppableProps}
-                  className={
-                    snapshot.isDraggingOver ? "columnColored" : "columnDefault"
-                  }
-                >
-                  {mappedCards}
-                  {provided.placeholder}
-                </div>
-              )}
-            </Droppable>
           </div>
+          <Droppable droppableId={props.column.columnId} type="card">
+            {(provided, snapshot) => (
+              <div
+                ref={provided.innerRef}
+                {...provided.droppableProps}
+                className={
+                  snapshot.isDraggingOver ? "columnColored" : "columnDefault"
+                }
+              >
+                {mappedCards}
+                {provided.placeholder}
+              </div>
+            )}
+          </Droppable>
         </div>
       )}
     </Draggable>
