06083ceab8514794b967cbe1de0a73c28af28024
Andrew Higgins <andrewhiggins27@gmail.com>
2021-06-08 14:06:06 -0400

    Added dnd capabilities to taskcard and columns

--- src/ui/TaskCard.js
+++ src/ui/TaskCard.js
@@ -1,5 +1,27 @@
 import React from "react";
 
+import { Draggable } from 'react-beautiful-dnd'
+
 export const TaskCard = (props) => {
-  return <div className="card">{props.card.title}</div>;
+  return(
+    <Draggable draggableId={props.card.id} index={props.index}>
+      {(provided, snapshot) => (
+        <div
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          ref={provided.innerRef}
+        >
+          <div className={(snapshot.isDragging ? "taskcard-color" : "taskcard-default")}>
+            <div className="card">
+              {props.card.title}
+            </div>
+
+          </div>
+        </div>
+      )}
+    </Draggable>
+
+
+
+  )
 };
